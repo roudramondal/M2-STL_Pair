544147b5aefb5b80d32d4f5454c831bb61ee16a7
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-21 15:36:40 +0600

    M2 (c-7) Pair declare and assign value ,output
    for output p.first and p.second

---
+++ pair_first.cpp
@@ -0,0 +1,38 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 21 / 2025
+   Time  : 15:31
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define YES cout << 'YES\n'
+#define Yes cout << 'Yes\n'
+#define NO cout << 'NO\n'
+#define No cout << 'No\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define all(v) v.begin(), v.end()
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    // code here 
+    pair<int,int> p={1,2};
+    cout<<p.first<<' '<<p.second<<nl;
+}
+
+int main() {
+    //fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
