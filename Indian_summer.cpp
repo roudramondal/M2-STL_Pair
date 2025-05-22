99e7e055a0137ae9739ac112db9ced6ad9bb60c4
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-22 14:35:02 +0600

    M2-7 codeforces print size
    taking input using set <pair<string,string>>v;

---
+++ Indian_summer.cpp
@@ -0,0 +1,39 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 22 / 2025
+   Time  : 14:21
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define YES cout << 'YES\n'
+#define Yes cout << 'Yes\n'
+#define NO cout << 'NO\n'
+#define No cout << 'No\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define all(v) v.begin(), v.end()
+#define pii pair<int,int>
+#define pll pair<<ll ,ll>
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+
+int main() {
+    fast_io;
+    set<pair<string,string>>v;
+    int n;
+    cin>>n;
+    for (int i=0;i<n;i++){
+        string a,b;
+        cin>>a>>b;
+        v.insert({a,b});
+    }
+    cout<<v.size()<<nl;
+    return 0;
+}
